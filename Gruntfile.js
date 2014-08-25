6f09c4e8a42aeab2f44831d4518d53200a6935b2
Artem Sotnikov <artem.sotnikov@undev.home>
2014-08-25 15:40:58 +0400

    set haml -> html task and watch tack #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    concat: { //Склейка скриптов
-    	dist:{
-    		src:[
-    			'assets/js/src/vendor/jquery-*.js',
-    			'assets/js/src/vendor/*.js',
-    			'assets/js/src/*.js'
-    		],
-    		dest: 'assets/js/build/production.js'
-    	}
-    },
+	grunt.initConfig({
+		concat: { //Склейка скриптов
+			dist:{
+				src:[
+					'assets/js/src/vendor/jquery-*.js',
+					'assets/js/src/vendor/*.js',
+					'assets/js/src/*.js'
+				],
+				dest: 'assets/js/build/production.js'
+			}
+		},
 
-    uglify: { //Сжатие скриптов
-	    build: {
-        src: 'assets/js/build/production.js',
-        dest: 'assets/js/build/production.min.js'
-	    }
+		uglify: { //Сжатие скриптов
+			build: {
+				src: 'assets/js/build/production.js',
+				dest: 'assets/js/build/production.min.js'
+			}
 		},
 
 		spritesheet:{ //Склейка спрайтов
@@ -24,19 +24,19 @@ module.exports = function(grunt) {
 					outputImage: 'images/build/spritesheet.png',
 					outputCss: 'styles/src/sprites.css',
 					selector: '.icon',
-          output: {
-            legacy: {
-              pixelRatio: 1,
-              outputImage: 'images/build/spritesheet.png'
-            },
-            retina: {
-              pixelRatio: 2,
-              outputImage: 'images/build/spritesheet@2x.png'
-            }
-	        },
-          resolveImageSelector: function( name, fullpath ) {
-            return name.split( "@2x" ).join( "" );
-          }
+					output: {
+						legacy: {
+							pixelRatio: 1,
+							outputImage: 'images/build/spritesheet.png'
+						},
+						retina: {
+							pixelRatio: 2,
+							outputImage: 'images/build/spritesheet@2x.png'
+						}
+					},
+					resolveImageSelector: function( name, fullpath ) {
+						return name.split( "@2x" ).join( "" );
+					}
 				},
 				files: {
 					'assets': 'assets/images/src/*'
@@ -45,39 +45,43 @@ module.exports = function(grunt) {
 		},
 
 		less: { //Компиляция less файлов
-		  dist: {
-		    options: {
-		      paths: ["assets/styles"]
-		    },
-		    files: {
-		      "assets/styles/src/importer.css": "assets/styles/src/importer.less"
-		    }
-		  },
+			dist: {
+				options: {
+					paths: ["assets/styles"]
+				},
+				files: {
+					"assets/styles/src/importer.css": "assets/styles/src/importer.less"
+				}
+			},
 		},
 
-	  cssjoin: { //Склейка стилей
-	    join :{
-	      files: {
-	        'assets/styles/build/production.css': ['assets/styles/src/*.css'],
-	      },
-	    }
-	  },
+		cssjoin: { //Склейка стилей
+			join :{
+				files: {
+					'assets/styles/build/production.css': ['assets/styles/src/*.css']
+				}
+			}
+		},
 
 		cssmin: { //Сжатие стилей
-		  combine: {
-		    files: {
-		      'assets/styles/build/production.min.css': 'assets/styles/build/production.css'
-		    }
-		  }
+			combine: {
+				files: {
+					'assets/styles/build/production.min.css': 'assets/styles/build/production.css'
+				}
+			}
 		},
-
-	  haml: {
-	    dist: {
-	      files: {
-	        '*.html': 'haml/*.haml'
-	      }
-	    }
-	  },
+	
+		allhaml: { //Сборка  haml шаблонов
+			options: {
+				inDir: 'haml',
+				outDir: '',
+				ouEx: 'html'
+			},
+			dist: {
+				src: ['<%= allhaml.options.inDir %>/**/*.haml'],
+				dest: '<%= allhaml.options.outDir %>'
+			}
+		},
 
 		//TODO: сделать нормальную склейку файлов после генерации спрайтов
 		watch: { 
@@ -86,39 +90,43 @@ module.exports = function(grunt) {
 				tasks: ['concat', 'uglify'],
 				options: {
 					spawn: false,
-				},
+				}
 			},
 			sprites:{
 				files: ['assets/images/src/*'],
 				tasks: ['spritesheet'],
 				options: {
 					spawn: false,
-				},
+				}
 			},
 			css: {
 				files: ['assets/styles/src/sprites.css','assets/styles/src/*','assets/styles/src/**/*.less'],
 				tasks: ['less', 'cssjoin' ,'cssmin'],
 				options: {
 					spawn: false,
-				},
+				}
 			},
-			cssjoin{
-				files: []
+			haml: {
+				files: ['haml/**/*.haml'],
+				tasks: ['allhaml'],
+				options: {
+					spawn: false,
+				}
 			}
 		}
 
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('node-spritesheet');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-cssjoin');
-	grunt.loadNpmTasks('grunt-contrib-haml');
-
+	grunt.loadNpmTasks('grunt-allhaml');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 
-  grunt.registerTask('default', ['concat', 'uglify', 'spritesheet', 'less', 'cssjoin' , 'cssmin', 'haml']);
+	grunt.registerTask('default', ['concat', 'uglify', 'spritesheet', 'less', 'cssjoin' , 'cssmin', 'allhaml']);
+	grunt.registerTask('haml', ['allhaml']);
 };
